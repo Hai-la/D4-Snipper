7120ad8ccad254fe7837e1c9f11e2b07d138f672
Hai-la <taylorjbaye@gmail.com>
2023-07-17 17:10:14 -0500

    Fixed scrollable rare detection

--- D4S.py
+++ D4S.py
@@ -4,30 +4,50 @@ import os
 
 def count_files(directory):
     file_count = 0
-    for _, _, files in os.walk(directory):
-        file_count += len(files)
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.isfile(file_path):
+                file_count += 1
     return file_count
 
 def screenshot_item():
     try:
-        # Look for item frame
-        TL_x,TL_y,w,h = pyautogui.locateOnScreen("img\\item_frame_TL_rare.png",confidence = 0.8)
-        BR_x,BR_y,w,h = pyautogui.locateOnScreen("img\\item_frame_BR_rare.png",confidence = 0.8)
+        # Look for normal rare item frame
+        TL_x,TL_y,_,_ = pyautogui.locateOnScreen("img\\item_frame_TL_rare.png",confidence = 0.8)
+        BR_x,BR_y,w2,_ = pyautogui.locateOnScreen("img\\item_frame_BR_rare.png",confidence = 0.8)
         print("Item found...")
 
         # Calculate width and height
-        w = BR_x + w - TL_x
+        w = BR_x + w2 - TL_x
         h = BR_y - TL_y
 
         # Take screenshot
         try:
-            img = pyautogui.screenshot("screenshots\\item_" + str(count_files("screenshots")) + ".png",region = (TL_x,TL_y,w,h))
-            print("screenshot saved")
-            print("item_" + str(count_files("screenshots")) + ".png")
+            file_num = str(count_files("screenshots")+1)
+            img = pyautogui.screenshot("screenshots\\item_" + file_num + ".png",region = (TL_x,TL_y,w,h))
+            print("Screenshot saved: "+"item_" + file_num + ".png")
         except ValueError:
             print("Error: Bottom right frame corner is further left or higher than top left frame corner.\nThis usually happens when there is more than one item frame on screen.")
     except TypeError:
-        print("Error: Item not found...")
+        try:
+            # Look for scrollable rare item frame
+            BR_x,BR_y,w2,h2 = pyautogui.locateOnScreen("img\\item_frame_BR_rare_scrollable.png",confidence = 0.8)
+            print("Item found")
+
+            # Calculate width and height
+            w = BR_x + w2 - TL_x
+            h = ((BR_y + h2) - 5) - TL_y
+
+            # Take screenshot
+            try:
+                file_num = str(count_files("screenshots")+1)
+                img = pyautogui.screenshot("screenshots\\item_" + file_num + ".png",region = (TL_x,TL_y,w,h))
+                print("Screenshot saved: "+"item_" + file_num + ".png")
+            except ValueError:
+                print("Error: Bottom right frame corner is further left or higher than top left frame corner.\nThis usually happens when there is more than one item frame on screen.")
+        except:
+            print("Error: Item not found...")
 
 def handle_screenshot(e):
     global snip_state
